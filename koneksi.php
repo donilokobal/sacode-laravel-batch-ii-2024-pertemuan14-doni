4e59e5a3d180b9525b973c37802c805d842bc405
Doni <tambah file html dan css>
2024-09-26 20:01:47 +0900

    lanjutan Pertemuan 15 - 14 crud

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "database_sekolah";
+$koneksi = mysqli_connect($host, $username, $password, $database);
+
+mysqil_select_db($koneksi, $database);
